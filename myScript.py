fc07b94e1f664a244764e141e4f51fd97d536f00
Ben DeBoer <github@eldeboero.com>
2025-03-09 17:05:41 -0700

    some stuff works

--- myScript.py
+++ myScript.py
@@ -1,26 +1,70 @@
-import nacl.utils
-from nacl.public import PrivateKey, Box
-
+import pyotp
+import time
 import json
+import qrcode
+import qrcode.image.svg
+import urllib.parse
+import uuid
 
+class Person:
+    def __init__(self, personName="", personId="", personKey=""):
+        self.name = personName
+        self.id = personId
+        self.key = personKey
+
+
+'''
+Main Lambda handler
+'''
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps(event)
-    }
+    if event['body']['operation'] == "verify":
+        return {
+            "statusCode": 200,
+            "body": verify(event['body']['id'], event['body']['totp'])
+        }
+    elif event['body']['operation'] == "showQRCode":
+        return {
+            "statusCode": 200,
+            "body": showQRCode(event['body']['id'])
+        }
 
+'''
+Verify -- returns True or False
+'''
+def verify(userId: str, submittedTOTP: str) -> bool:
+    person = getPersonById(userId)
+    personTOTP = pyotp.TOTP(person.key)
+    print(f'Expected TOTP: {personTOTP.now()}')
+    print(f'User submitted TOTP: {submittedTOTP}')
+    
+    return personTOTP.verify(submittedTOTP)
 
-testValidationPayload = {
-    "operation":"validate",
-    "totp":"12345"
-    }
+'''
+showQRCode - returns an html page with embedded QR code
+'''
 
-testEvent = {
-    "body": testValidationPayload
-}
+def showQRCode(userId: str) -> str:
+    person = getPersonById(userId)
+    safePersonName = urllib.parse.quote_plus(person.name)
+    uri = "otpauth://totp/RealityCheck:" + safePersonName + "?secret=" + person.key + "&issuer=RealityCheck"
+    factory = qrcode.image.svg.SvgPathImage
+    img = qrcode.make(uri, image_factory=factory)
+    html = "<html><head><title>Temporary QR code</title></head><body>" + img.to_string(encoding='unicode') + "</body></html>"
+    return html
 
-testContext = {
-    "functionName":"Test Function"
-}
+def getPersonById(userId: str) -> Person:
 
-print(lambda_handler(testEvent, testContext))
+    '''
+    This function needs to be replaced with something that uses a secure storage
+    '''
+
+    ben = Person("Ben DeBoer", "ben_id", "fakekey1")
+    bob = Person("Bob Jones", "bob_id", "fakekey2")
+    people = [ben, bob]
+
+    for person in people:
+        if person.id == userId:
+            return person
+
+def registerPerson(userName: str) -> str:
+    ...
